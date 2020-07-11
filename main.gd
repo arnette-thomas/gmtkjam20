db8827d0c795171807766a65af06d443c44782c5
arnette-thomas <thomas.arnette@outlook.fr>
2020-07-11 21:26:53 +0200

    Change cursor behaviour

--- main.gd
+++ main.gd
@@ -6,22 +6,9 @@ extends Node2D
 # var b = "text"
 
 
-var cursor = preload("res://cursor.tscn")
+# var cursor = preload("res://scenes/cursor.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-var node
-func _input(event):
-	var mouse_event = event as InputEventMouseButton
-	if mouse_event:
-		if mouse_event.pressed:
-			node = cursor.instance()
-			node.position = mouse_event.position
-			if mouse_event.button_index == BUTTON_LEFT:
-				node.coef = -1
-			elif mouse_event.button_index == BUTTON_RIGHT:
-				node.coef = 1
-			
-			add_child(node)
---
+++ scenes/cursor.gd
@@ -0,0 +1,33 @@
+class_name AttracEffect
+
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var coef = 1
+var active = false
+
+func _process(delta):
+	self.position = get_global_mouse_position()
+	var attract_mode = Input.is_action_pressed("attract")
+	if attract_mode || Input.is_action_pressed("repulse"):
+		coef = abs(coef) * (-1 if attract_mode else 1)
+		active = true
+	else:
+		active = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	if active:
+		for body in get_overlapping_bodies():
+			var person = body as Person
+			if person:
+				var diff = person.position - position
+				person.add_effect(coef * diff.normalized())
